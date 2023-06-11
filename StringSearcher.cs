f89b774d3716f431fb0409288199f2df69e9bf94
ekaterinamusatova <91495066+ekaterinamusatova@users.noreply.github.com>
2023-06-11 20:20:15 +0500

    /

---
+++ StringSearcher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace Task5
+{
+    public class StringSearcher
+    {
+        public string[] Search(string[] array)
+        {
+            return array.Where(s => s.Length > 0)
+                .Select(s => s.Substring(Math.Min(3, s.Length)).ToUpper())
+                .Distinct()
+                .OrderByDescending(s => s)
+                .ToArray();
+        }
+    }
+}
