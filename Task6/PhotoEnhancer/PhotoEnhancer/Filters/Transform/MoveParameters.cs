58b926ceaa6b1ef0aea576c09712af0df2684baa
ekaterinamusatova <91495066+ekaterinamusatova@users.noreply.github.com>
2023-06-13 21:48:31 +0500

    /

---
+++ Task6/PhotoEnhancer/PhotoEnhancer/Filters/Transform/MoveParameters.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoEnhancer
+{
+    public class MoveParameters : IParameters
+    {
+        [ParameterInfo(Name = "Сдвиг на (%)",
+                    MinValue = 0,
+                    MaxValue = 100,
+                    DefaultValue = 0,
+                    Increment = 5)]
+
+        public double MoveInPercent { get; set; }
+    }
+}
---
+++ Task6/PhotoEnhancer/PhotoEnhancer/Filters/Transform/MoveTransformer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoEnhancer
+{
+    public class MoveTransformer : ITransformer<MoveParameters>
+    {
+        double movePercent { get; set; }
+
+        public Size ResultSize { get; private set; }
+
+        public void Initialize(Size size, MoveParameters parameters)
+        {
+            movePercent = parameters.MoveInPercent / 100;
+            ResultSize = size;
+        }
+
+        public Point? MapPoint(Point point)
+        {
+            return new Point(point.X, (point.Y + (int)(movePercent * ResultSize.Height)) % ResultSize.Height);
+        }
+    }
+}
